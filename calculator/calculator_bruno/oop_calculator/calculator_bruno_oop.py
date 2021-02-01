7f960b75ffdcb8cc49e9c7283a11d6e1ce9c3f08
Bruno <brunonovo18@gmail.com>
2021-02-01 20:42:32 -0300

    Adding new documentation, fix lint errors

--- calculator/calculator_bruno/oop_calculator/calculator_bruno_oop.py
+++ calculator/calculator_bruno/oop_calculator/calculator_bruno_oop.py
@@ -1,4 +1,8 @@
-from tkinter import *
+'''
+DOCSTRING: OOP Calculator using Python
+'''
+
+from tkinter import Tk
 from calculator_class import Calculator
 
 #Main Execution Script
@@ -10,5 +14,8 @@ if __name__=='__main__':
     #Tell our calculator class to use this window
     main_screen = Calculator(window)
 
+    window.geometry("400x200")
+    window.minsize(width=400, height=200)
+    window.maxsize(width=600, height=300)
     #Executable loop on the application, waits for user input
     window.mainloop()
--- calculator/calculator_bruno/oop_calculator/calculator_class.py
+++ calculator/calculator_bruno/oop_calculator/calculator_class.py
@@ -1,8 +1,15 @@
-from tkinter import *
+'''
+DOCSTRING: OOP Calculator using Python
+'''
+
+import tkinter as tk
 
 ## Create the calculator class
 class Calculator:
 
+    '''
+        DOCSTRING: Class Calculator definition
+    '''
 
     def __init__(self, master):
 
@@ -17,17 +24,17 @@ class Calculator:
         master.title("Python Calculator")
 
         #Create a line where we display the equation
-        self.equation=Entry(master, width=36, borderwidth=5)
+        self.equation=tk.Entry(master, width=36, borderwidth=5)
 
         #Assign a position for the equation line in the grey application window
         self.equation.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
 
         #Execute the .creteButton() method
-        self.createButton()
+        self.create_button()
 
 
 
-    def createButton(self):
+    def create_button(self):
 
         '''
         DOCSTRING: Method that creates the buttons
@@ -36,53 +43,51 @@ class Calculator:
         '''
 
         #We first create each button one by one with the value we want
-        #Using addButton() method which is described below
-        b0 = self.addButton(0)
-        b1 = self.addButton(1)
-        b2 = self.addButton(2)
-        b3 = self.addButton(3)
-        b4 = self.addButton(4)
-        b5 = self.addButton(5)
-        b6 = self.addButton(6)
-        b7 = self.addButton(7)
-        b8 = self.addButton(8)
-        b9 =  self.addButton(9)
-        b_add = self.addButton('+')
-        b_sub = self.addButton('-')
-        b_mult = self.addButton('*')
-        b_div = self.addButton('/')
-        b_clear = self.addButton('c')
-        b_equal = self.addButton('=')
+        #Using add_button() method which is described below
+        button_0 = self.add_button(0)
+        button_1 = self.add_button(1)
+        button_2 = self.add_button(2)
+        button_3 = self.add_button(3)
+        button_4 = self.add_button(4)
+        button_5 = self.add_button(5)
+        button_6 = self.add_button(6)
+        button_7 = self.add_button(7)
+        button_8 = self.add_button(8)
+        button_9 =  self.add_button(9)
+        button_add = self.add_button('+')
+        button_sub = self.add_button('-')
+        button_mult = self.add_button('*')
+        button_div = self.add_button('/')
+        button_clear = self.add_button('c')
+        button_equal = self.add_button('=')
 
         #Arrange the buttons into lists which represent calculator rows
-        row1=[b7,b8,b9,b_add]
-        row2=[b4,b5,b6,b_sub]
-        row3=[b1,b2,b3,b_mult]
-        row4=[b_clear,b0,b_equal,b_div]
+        row1=[button_7,button_8,button_9,button_add]
+        row2=[button_4,button_5,button_6,button_sub]
+        row3=[button_1,button_2,button_3,button_mult]
+        row4=[button_clear,button_0,button_equal,button_div]
 
         #Assign each button to a particular location on the GUI
-        r=1
+        count_row=1
         for row in [row1, row2, row3, row4]:
-            c=0
+            count=0
             for buttn in row:
-                buttn.grid(row=r, column=c, columnspan=1)
-                c+=1
-            r+=1
+                buttn.grid(row=count_row, column=count, columnspan=1)
+                count+=1
+            count_row+=1
 
 
-
-    def addButton(self,value):
+    def add_button(self,value):
 
         '''
         DOCSTRING: Method to process the creation of a button and make it clickable
         INPUT: value of the button (1,2,3,4,5,6,7,8,9,0,+,-,*,/,c,=)
         OUTPUT: returns a designed button object
         '''
-        return Button(self.master, text=value, width=9, command = lambda: self.clickButton(str(value)))
+        return tk.Button(self.master,text=value,width=9,command=lambda: self.click_button(str(value)))
 
 
-
-    def clickButton(self, value):
+    def click_button(self, value):
 
         '''
         DOCSTRING: Method to program the actions that will happen in the calculator after a click of each button
@@ -95,16 +100,15 @@ class Calculator:
 
         #If user clicked "c", then clear the screen
         if value == 'c':
-            self.equation.delete(-1, END)
+            self.equation.delete(-1, tk.END)
 
         #If user clicked "=", then compute the answer and display it
         elif value == '=':
             answer = str(eval(current_equation))
-            self.equation.delete(-1, END)
+            self.equation.delete(-1, tk.END)
             self.equation.insert(0, answer)
 
         #If user clicked any other button, then add it to the equation line
         else:
-            self.equation.delete(0, END)
+            self.equation.delete(0, tk.END)
             self.equation.insert(0, current_equation+value)
-

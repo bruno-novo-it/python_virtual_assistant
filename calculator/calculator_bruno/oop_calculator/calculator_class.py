d05c5564093399cee4af416473160c01051be0e7
Bruno <brunonovo18@gmail.com>
2021-02-01 22:08:48 -0300

    Adding green color to the equals button

--- calculator/calculator_bruno/oop_calculator/calculator_class.py
+++ calculator/calculator_bruno/oop_calculator/calculator_class.py
@@ -31,7 +31,7 @@ class Calculator:
         # Assign a position for the equation line in the grey application window
         self.equation.grid(row=0, column=0, columnspan=5, padx=10, pady=10)
 
-        # Execute the .creteButton() method
+        # Execute the .create_button() method
         self.create_button()
 
 
@@ -44,7 +44,7 @@ class Calculator:
         OUTPUT: creates a button
         '''
 
-        #Arrange the buttons into lists which represent calculator rows
+        # Arrange the buttons into lists which represent calculator rows
         row1 = [
                 self.add_button(7),
                 self.add_button(8),
@@ -74,12 +74,12 @@ class Calculator:
                 self.add_button('=')
         ]
 
-        #Assign each button to a particular location on the GUI
+        # Assign each button to a particular location on the GUI
         count_row=1
         for row in [row1, row2, row3, row4]:
             count=0
-            for buttn in row:
-                buttn.grid(row=count_row, column=count, columnspan=1)
+            for button in row:
+                button.grid(row=count_row, column=count, columnspan=1)
                 count+=1
             count_row+=1
 
@@ -88,10 +88,18 @@ class Calculator:
 
         '''
         DOCSTRING: Method to process the creation of a button and make it clickable
-        INPUT: value of the button (1,2,3,4,5,6,7,8,9,0,+,-,*,/,c,=)
+        INPUT: value of the button (1,2,3,4,5,6,7,8,9,0,+,%,",",-,*,/,c,=)
         OUTPUT: returns a designed button object
         '''
-        return tk.Button(self.master,text=value,width=9, \
+        if value == '=':
+            background_color = "green"
+            active_background_color = "green"
+        else:
+            background_color = None
+            active_background_color = None
+
+        return tk.Button(self.master,text=value,width=9,bg=background_color, \
+            activebackground=active_background_color, \
             command=lambda:self.click_button(str(value)))
 
 
@@ -99,18 +107,18 @@ class Calculator:
 
         '''
         DOCSTRING: Method that will happen in the calculator after a click of each button
-        INPUT: value of the button (1,2,3,4,5,6,7,8,9,0,+,-,*,/,c,=)
+        INPUT: value of the button (1,2,3,4,5,6,7,8,9,0,+,%,",",-,*,/,c,=)
         OUTPUT: what action will be performed when a particular button is clicked
         '''
 
-        #Get the equation that's entered by the user
+        # Get the equation that's entered by the user
         current_equation=str(self.equation.get())
 
-        #If user clicked "c", then clear the screen
+        # If user clicked "c", then clear the screen
         if value == 'c':
             self.equation.delete(-1, tk.END)
 
-        #If user clicked "=", then compute the answer and display it
+        # If user clicked "=", then compute the answer and display it
         elif value == '=':
             if "%" in current_equation:
                 answer = str(eval(current_equation.replace("%","/100")))
@@ -119,7 +127,7 @@ class Calculator:
             self.equation.delete(-1, tk.END)
             self.equation.insert(0, answer)
 
-        #If user clicked any other button, then add it to the equation line
+        # If user clicked any other button, then add it to the equation line
         else:
             self.equation.delete(0, tk.END)
             self.equation.insert(0, current_equation+value)

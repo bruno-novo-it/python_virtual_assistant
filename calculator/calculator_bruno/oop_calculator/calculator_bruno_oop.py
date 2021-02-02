1740e893f89f0c66860b65f96bfdd3fd1922687c
Bruno <brunonovo18@gmail.com>
2021-02-01 21:48:03 -0300

    Adding new identation to the code and fixing display line

--- calculator/calculator_bruno/oop_calculator/calculator_bruno_oop.py
+++ calculator/calculator_bruno/oop_calculator/calculator_bruno_oop.py
@@ -8,14 +8,15 @@ from calculator_class import Calculator
 #Main Execution Script
 if __name__=='__main__':
 
-    #This will create the main window for the application
+    # This will create the main window for the application
     window = Tk()
 
-    #Tell our calculator class to use this window
+    # Tell our calculator class to use this window
     main_screen = Calculator(window)
 
-    window.geometry("400x200")
-    window.minsize(width=400, height=200)
-    window.maxsize(width=600, height=300)
-    #Executable loop on the application, waits for user input
+    # Settung Windows Min and Max size
+    window.minsize(width=500, height=200)
+    window.maxsize(width=800, height=300)
+
+    # Executable loop on the application, waits for user input
     window.mainloop()
--- calculator/calculator_bruno/oop_calculator/calculator_class.py
+++ calculator/calculator_bruno/oop_calculator/calculator_class.py
@@ -19,19 +19,19 @@ class Calculator:
         DOCSTRING: Define what to do on initialization
         '''
 
-        #Assign reference to the main window of the application
+        # Assign reference to the main window of the application
         self.master = master
 
-        #Add a name to our application
+        # Add a name to our application
         master.title("Python Calculator")
 
-        #Create a line where we display the equation
-        self.equation=tk.Entry(master, width=36, borderwidth=5)
+        # Create a line where we display the equation
+        self.equation=tk.Entry(master, width=50, borderwidth=5)
 
-        #Assign a position for the equation line in the grey application window
-        self.equation.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
+        # Assign a position for the equation line in the grey application window
+        self.equation.grid(row=0, column=0, columnspan=5, padx=10, pady=10)
 
-        #Execute the .creteButton() method
+        # Execute the .creteButton() method
         self.create_button()
 
 

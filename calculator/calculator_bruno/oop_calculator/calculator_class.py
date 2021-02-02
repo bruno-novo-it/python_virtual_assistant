df01dfd2396926aa2202367faadb4305466a59b2
Bruno <brunonovo18@gmail.com>
2021-02-01 21:03:58 -0300

    Removing unnecessary variables

--- calculator/calculator_bruno/oop_calculator/calculator_class.py
+++ calculator/calculator_bruno/oop_calculator/calculator_class.py
@@ -42,30 +42,11 @@ class Calculator:
         OUTPUT: creates a button
         '''
 
-        #We first create each button one by one with the value we want
-        #Using add_button() method which is described below
-        button_0 = self.add_button(0)
-        button_1 = self.add_button(1)
-        button_2 = self.add_button(2)
-        button_3 = self.add_button(3)
-        button_4 = self.add_button(4)
-        button_5 = self.add_button(5)
-        button_6 = self.add_button(6)
-        button_7 = self.add_button(7)
-        button_8 = self.add_button(8)
-        button_9 =  self.add_button(9)
-        button_add = self.add_button('+')
-        button_sub = self.add_button('-')
-        button_mult = self.add_button('*')
-        button_div = self.add_button('/')
-        button_clear = self.add_button('c')
-        button_equal = self.add_button('=')
-
         #Arrange the buttons into lists which represent calculator rows
-        row1=[button_7,button_8,button_9,button_add]
-        row2=[button_4,button_5,button_6,button_sub]
-        row3=[button_1,button_2,button_3,button_mult]
-        row4=[button_clear,button_0,button_equal,button_div]
+        row1=[self.add_button(7),self.add_button(8),self.add_button(9),self.add_button('+')]
+        row2=[self.add_button(4),self.add_button(5),self.add_button(6),self.add_button('-')]
+        row3=[self.add_button(1),self.add_button(2),self.add_button(3),self.add_button('*')]
+        row4=[self.add_button('c'),self.add_button(0),self.add_button('='),self.add_button('/')]
 
         #Assign each button to a particular location on the GUI
         count_row=1
@@ -84,13 +65,14 @@ class Calculator:
         INPUT: value of the button (1,2,3,4,5,6,7,8,9,0,+,-,*,/,c,=)
         OUTPUT: returns a designed button object
         '''
-        return tk.Button(self.master,text=value,width=9,command=lambda: self.click_button(str(value)))
+        return tk.Button(self.master,text=value,width=9, \
+            command=lambda:self.click_button(str(value)))
 
 
     def click_button(self, value):
 
         '''
-        DOCSTRING: Method to program the actions that will happen in the calculator after a click of each button
+        DOCSTRING: Method that will happen in the calculator after a click of each button
         INPUT: value of the button (1,2,3,4,5,6,7,8,9,0,+,-,*,/,c,=)
         OUTPUT: what action will be performed when a particular button is clicked
         '''
